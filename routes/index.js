fe1f6522db009e35836a02e6392be2559e0aae2f
Matt Andrews <matt@mattandrews.info>
2015-12-01 23:22:01 +0000

    Experimental template import

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,15 @@ var router = express.Router();
 var knex = require('../database');
 
 router.get('/', function(req, res) {
+
     var news = knex.select('headline_text', 'date_posted').from('news').limit(10);
+
     var reviews = knex.select('band_name', 'record_name')
         .join('records', 'reviews.record_id', 'records.id')
         .join('bands', 'records.band_id', 'bands.id')
         .from('reviews').limit(10);
-    res.render('index', {
+
+    res.render('homepage', {
         title: 'Express',
         news: news,
         reviews: reviews
